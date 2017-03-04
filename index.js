b79278d69f91db347fc1f27627b582aaf2cc271b
Jess Telford <jess+github@jes.st>
2017-03-05 01:34:31 +1100

    File RPC commands

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const path = require('path')
 const http = require('http')
 const Primus = require('primus')
@@ -8,6 +9,33 @@ const httpShutdown = require('http-shutdown')
 const primusConfig = require('./primus-config')
 
 const PORT = 51000
+
+function getFileHash (filePath) {
+  return 'TODO: md5hash'
+}
+
+function outputConflict (filename, localContents, remoteContents) {
+  // TODO: Use a diff algorithm to find what's changed, then console.error
+  // it out with a message about refusing to merge
+  console.log(`Conflict in ${filename}:\nLocal contents:\n\n${localContents}\n\nRemote Contents:\n\n${remoteContents}`)
+}
+
+function handleConflict (filename, remoteContents) {
+  // TODO: Check file exists first. If not, just write it out (conflict
+  // resolved!)
+  fs.readFileSync(filename, (error, localContents) => {
+    if (error) {
+      // TODO: Better error message
+      console.error(error)
+      return
+    }
+    outputConflict(filename, localContents, remoteContents)
+  })
+}
+
+function watch (path, callback) {
+  callback()
+}
 
 module.exports = function server ({scripts, port = PORT} = {}) {
   // Accept both relative & absolute paths, defaulting to `process.cwd()`
@@ -28,9 +56,67 @@ module.exports = function server ({scripts, port = PORT} = {}) {
   const socketServer = new Primus(httpServer, primusConfig)
 
   socketServer.on('connection', (spark) => {
-    spark.on('data', (data) => {
-      // TODO
-      console.log('received data from the client', data)
+    // Immediately trigger a sync
+    spark.emit('sync', {
+      'doit.js': getFileHash('doit.js')
+    })
+
+    spark.on('conflict', ({filename, contents}) => {
+      handleConflict(filename, contents)
+    })
+
+    spark.on('sync', (files) => {
+      for (let file in files) {
+        if (!files.hasOwnProperty(file)) {
+          continue
+        }
+
+        if (!files[file]) {
+          // No hash provided, so can't compare files
+          continue
+        }
+
+        const localHash = getFileHash(file)
+
+        if (!localHash) {
+          // Unknown local file, request contents
+          spark.emit('get-file', file)
+          return
+        }
+
+        if (localHash !== files[file]) {
+          handleConflict(file)
+        }
+      }
+    })
+
+    spark.on('file', ({filename, contents}) => {
+      // TODO: `filename` validation
+      // TODO: `filename` must be relative to scripts path
+      fs.writeFile(filename, contents)
+    })
+
+    spark.on('get-file', (filename) => {
+      fs.readFile(filename, (error, contents) => {
+        if (error) {
+          // TODO: Better error
+          console.error(error)
+        }
+        spark.emit('file', {filename, contents})
+      })
+    })
+
+    // Callback triggered on every file change
+    // TODO: `watch` function
+    // TODO: correct glob relative to scripts directory
+    watch('/**/*.js', (filename) => {
+      fs.readFile(filename, (error, contents) => {
+        if (error) {
+          // TODO: Better error
+          console.error(error)
+        }
+        spark.emit('file', {filename, contents})
+      })
     })
   })
 
--- primus-config.js
+++ primus-config.js
@@ -1,5 +1,8 @@
 module.exports = {
   transformer: 'websockets', // Pure JS, doesn't need C++ toolchain to compile
   pathname: '/primus',
-  iknowhttpsisbetter: true // We'll only ever run this on `localhost`, so no certs
+  iknowhttpsisbetter: true, // We'll only ever run this on `localhost`, so no certs
+  plugin: {
+    'events': require('primus-emit')
+  }
 }
