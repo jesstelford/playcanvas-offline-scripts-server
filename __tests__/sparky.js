1ac6b3144cf698a272d7110dd53dc04a65dcf1c1
Jess Telford <jess+github@jes.st>
2017-03-05 20:48:40 +1100

    Outlining tests for websockets

---
+++ __tests__/sparky.js
@@ -0,0 +1,44 @@
+/* eslint-env jest */
+
+describe.skip('Streaming incoming files', () => {
+  it('handles new files', () => {
+
+  })
+
+  it('handles edited files', () => {
+
+  })
+
+  it('handles removed files', () => {
+
+  })
+})
+
+describe.skip('Streaming local file changes', () => {
+  describe('File add', () => {
+    it('triggers a stream', () => {
+
+    })
+    it('streams the file contents', () => {
+
+    })
+  })
+
+  describe('File change', () => {
+    it('triggers a stream', () => {
+
+    })
+    it('streams the file contents', () => {
+
+    })
+  })
+
+  describe('File remove', () => {
+    it('triggers a stream', () => {
+
+    })
+    it('has no file contents to stream', () => {
+
+    })
+  })
+})
